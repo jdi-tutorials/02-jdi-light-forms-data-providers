e185bee90de3ae35f43ae116cabbe77e42f1e270
RomanIovlev <Roman_Iovlev@epam.com>
2019-04-22 16:53:50 +0300

    update testst

--- src/main/java/jdisite/JDISite.java
+++ src/main/java/jdisite/JDISite.java
@@ -2,7 +2,6 @@ package jdisite;
 
 import com.epam.jdi.light.elements.pageobjects.annotations.JSite;
 import com.epam.jdi.light.elements.pageobjects.annotations.simple.UI;
-import com.epam.jdi.light.ui.html.common.Button;
 import com.epam.jdi.light.ui.html.common.Link;
 import com.epam.jdi.light.ui.html.common.Text;
 import com.epam.jdi.light.ui.html.complex.Menu;
@@ -19,7 +18,6 @@ public class JDISite {
 
     @UI("img#user-icon") public static Link userIcon;
     @UI("#user-name") public static Text userName;
-    @UI(".fa-sign-out") public static Button logout;
     @UI(".sidebar-menu span") public static Menu sideMenu;
 
 }
